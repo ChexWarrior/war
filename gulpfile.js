a33b4ec72e93df06a79769fdef1964aead92cd79
Andrew Lehman <aplehm@gmail.com>
2017-03-05 17:50:19 -0500

    Create tasks for sasa and js and one for both

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
-gulp.task('build', () => {
+gulp.task('style', () => {
+  return gulp.src('app/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/styles/'));
+});
+
+gulp.task('script', () => {
   return gulp.src('app/scripts/src/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
@@ -11,3 +18,5 @@ gulp.task('build', () => {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('app/scripts/compiled'));
 });
+
+gulp.task('build', ['style','script']);
