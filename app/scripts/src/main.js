1668667903dd6092a457518874b2b3236f61513b
Andrew Lehman <aplehm@gmail.com>
2017-03-06 08:40:35 -0500

    Change image based on card drawn

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -2,19 +2,33 @@
     
  Vue.component('card-table', {
   template: `
-    <div class="card-table">\
-      <pile currentCardSrc="https://deckofcardsapi.com/static/img/8C.png"></pile>
+    <div class="card-table">
+      <span>Deck ID: {{ deck_id }} Cards Remaining: {{ remaining }}</span>
+      <pile v-bind:currentCardSrc="drawnCardUrl"></pile>
+      <button v-on:click="drawCard">Draw a Card</button>
     </div>`,
+  beforeCreate: function() {
+    DoC.createDeck({ shuffled: true }).then((response) => {
+        this.deck_id = response.deck_id;
+        this.remaining = response.remaining;
+    });
+  },
   data: function() {
     return {
-      'deck_id': false,
+      'deck_id': 'not set',
       'remaining': 0,
-      'piles': {}
+      'drawnCardUrl': 'https://deckofcardsapi.com/static/img/8C.png'
     };
   },
   methods: {
     drawCard: function() {
-
+      DoC.drawFromDeck({
+        deckID: this.deck_id,
+        numCards: 1
+      }).then((response) => {
+        console.dir(response);
+        this.drawnCardUrl = response.cards[0].image;
+      });
     }
   }
  });
