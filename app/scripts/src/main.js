671c972e414ebbfaa7d16e74a131f1f94792aeff
Andrew Lehman <aplehm@gmail.com>
2017-03-06 08:26:10 -0500

    Add initial vue components

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -1,15 +1,34 @@
 ((DoC, Vue) => {
     
-  let app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue.js!'
-    },
-    methods: {
-      reverseMessage: function() {
-        this.message = this.message.split('').reverse().join('')
-      }
+ Vue.component('card-table', {
+  template: `
+    <div class="card-table">\
+      <pile currentCardSrc="https://deckofcardsapi.com/static/img/8C.png"></pile>
+    </div>`,
+  data: function() {
+    return {
+      'deck_id': false,
+      'remaining': 0,
+      'piles': {}
+    };
+  },
+  methods: {
+    drawCard: function() {
+
     }
-  });
+  }
+ });
+
+ Vue.component('pile', {
+  template: `
+    <div class="pile">
+      <img v-bind:src="currentCardSrc"/>
+    </div>`,
+  props: ['currentCardSrc']
+ });
+
+ let app = new Vue({
+  el: '#app'
+ });
 
 })(window.DoC, window.Vue);
