eb03cf25be9116918804ae7b72f4ea5607949265
Andrew Lehman <aplehm@gmail.com>
2017-04-24 07:28:39 -0400

    Wrap components in IFFE

--- app/scripts/src/card-table.js
+++ app/scripts/src/card-table.js
@@ -1,115 +1,117 @@
- Vue.component('card-table', {
-  template: `
-    <div class="card-table">
-      <button class="start-button" 
-              v-on:click="startGame"
-              v-if="disabled">Click to Start</button>
-      <div v-bind:class="disabled ? 'game-suspended' : ''">
-        <span>Deck ID: {{ deckID }}</span>
-        <pile v-bind:enabled="pile1Enabled"
-              v-bind:deckID="deckID"
-              v-on:drawn="startMatch"
-              name="pile1"></pile>
-        <br/>
-        <pile v-bind:enabled="pile2Enabled"
-              v-bind:deckID="deckID"
-              v-on:drawn="startMatch"
-              name="pile2"></pile>
-      </div>
-    </div>`,
-  beforeCreate: async function() {
-    const deck = await DoC.createDeck({ shuffle: true });
-    this.deckID = deck.deck_id;
-    this.disabled = deck.success;
-  },
-  data: function() {
-    return {
-      'disabled': true,
-      'deckID': null,
-      'pile1Enabled': false,
-      'pile2Enabled': false
-    };
-  },
-  methods: {
-    startGame: function() {
-      this.disabled = false;
-      this.createDrawPiles({
-        deckID: this.deckID,
-        pileName1: 'pile1',
-        pileName2: 'pile2',
-        numCards: 26
-      });
+(function(Vue, DoC) {
+  Vue.component('card-table', {
+    template: `
+      <div class="card-table">
+        <button class="start-button" 
+                v-on:click="startGame"
+                v-if="disabled">Click to Start</button>
+        <div v-bind:class="disabled ? 'game-suspended' : ''">
+          <span>Deck ID: {{ deckID }}</span>
+          <pile v-bind:enabled="pile1Enabled"
+                v-bind:deckID="deckID"
+                v-on:drawn="startMatch"
+                name="pile1"></pile>
+          <br/>
+          <pile v-bind:enabled="pile2Enabled"
+                v-bind:deckID="deckID"
+                v-on:drawn="startMatch"
+                name="pile2"></pile>
+        </div>
+      </div>`,
+    beforeCreate: async function() {
+      const deck = await DoC.createDeck({ shuffle: true });
+      this.deckID = deck.deck_id;
+      this.disabled = deck.success;
     },
-    translateValue: function(value) {
-      switch(value) {
-        case 'ACE':
-          return 14;
-        break;
-        case 'KING':
-          return 13;
-        break;
-        case 'QUEEN':
-          return 12;
-        break;
-        case 'JACK':
-          return 11;
-        break;
-        default:
-          return parseInt(value, 10);
-      }
+    data: function() {
+      return {
+        'disabled': true,
+        'deckID': null,
+        'pile1Enabled': false,
+        'pile2Enabled': false
+      };
     },
-    startMatch: function(event) {
-      //TODO handling of cards after match
-      //TODO handle tie
-      console.log('Event Received!', event);
-      if(event.pile == 'pile1') {
-        this.pile1Card = this.translateValue(event.value);
-        this.pile1Enabled = false;
-      } else if(event.pile == 'pile2') {
-        this.pile2Card = this.translateValue(event.value);
-        this.pile2Enabled = false;
-      }
-
-      if(this.pile1Card && this.pile2Card) {
-        this.pile1Enabled = true;
-        this.pile2Enabled = true;
-        console.log('Both piles have drawn a card!');
-        if(this.pile1Card > this.pile2Card) {
-          console.log('Player 1 wins!');
-        } else if (this.pile2Card > this.pile1Card) {
-          console.log('Player 2 wins!');
-        } else {
-          console.log('Tie!');
+    methods: {
+      startGame: function() {
+        this.disabled = false;
+        this.createDrawPiles({
+          deckID: this.deckID,
+          pileName1: 'pile1',
+          pileName2: 'pile2',
+          numCards: 26
+        });
+      },
+      translateValue: function(value) {
+        switch(value) {
+          case 'ACE':
+            return 14;
+          break;
+          case 'KING':
+            return 13;
+          break;
+          case 'QUEEN':
+            return 12;
+          break;
+          case 'JACK':
+            return 11;
+          break;
+          default:
+            return parseInt(value, 10);
         }
-      } else {
-        console.log('Both piles have not drawn a card!');
+      },
+      startMatch: function(event) {
+        //TODO handling of cards after match
+        //TODO handle tie
+        console.log('Event Received!', event);
+        if(event.pile == 'pile1') {
+          this.pile1Card = this.translateValue(event.value);
+          this.pile1Enabled = false;
+        } else if(event.pile == 'pile2') {
+          this.pile2Card = this.translateValue(event.value);
+          this.pile2Enabled = false;
+        }
+
+        if(this.pile1Card && this.pile2Card) {
+          this.pile1Enabled = true;
+          this.pile2Enabled = true;
+          console.log('Both piles have drawn a card!');
+          if(this.pile1Card > this.pile2Card) {
+            console.log('Player 1 wins!');
+          } else if (this.pile2Card > this.pile1Card) {
+            console.log('Player 2 wins!');
+          } else {
+            console.log('Tie!');
+          }
+        } else {
+          console.log('Both piles have not drawn a card!');
+        }
+      },
+      createDrawPiles: async function(parameters) {
+        let deck = await DoC.drawFromDeck({
+          deckID: parameters.deckID,
+          numCards: parameters.numCards
+        });
+
+        const pile1 = await DoC.addToPile({
+          pileName: parameters.pileName1,
+          cardsToAdd: deck.cards,
+          deckID: parameters.deckID
+        });
+
+        this.pile1Enabled = pile1.success;
+        deck = await DoC.drawFromDeck({
+          deckID: parameters.deckID,
+          numCards: parameters.numCards
+        });
+
+        const pile2 = await DoC.addToPile({
+          pileName: parameters.pileName2,
+          cardsToAdd: deck.cards,
+          deckID: parameters.deckID
+        });
+
+        this.pile2Enabled = pile2.success;
       }
-    },
-    createDrawPiles: async function(parameters) {
-      let deck = await DoC.drawFromDeck({
-        deckID: parameters.deckID,
-        numCards: parameters.numCards
-      });
-
-      const pile1 = await DoC.addToPile({
-        pileName: parameters.pileName1,
-        cardsToAdd: deck.cards,
-        deckID: parameters.deckID
-      });
-
-      this.pile1Enabled = pile1.success;
-      deck = await DoC.drawFromDeck({
-        deckID: parameters.deckID,
-        numCards: parameters.numCards
-      });
-
-      const pile2 = await DoC.addToPile({
-        pileName: parameters.pileName2,
-        cardsToAdd: deck.cards,
-        deckID: parameters.deckID
-      });
-
-      this.pile2Enabled = pile2.success;
     }
-  }
- });
+  });
+}(window.Vue, window.DoC));
--- app/scripts/src/pile.js
+++ app/scripts/src/pile.js
@@ -1,41 +1,43 @@
- Vue.component('pile', {
-  template: `
-    <div>
-      <span>Pile Name: {{ name }}</span>
-      <span>Cards Remaining: {{ remaining }}</span>
-      <div v-on:click="drawCard" class="pile"></div>
-      <div class="current-card">
-        <img v-bind:src="drawnCardUrl"/>
+(function(Vue, DoC) {
+  Vue.component('pile', {
+    template: `
+      <div>
+        <span>Pile Name: {{ name }}</span>
+        <span>Cards Remaining: {{ remaining }}</span>
+        <div v-on:click="drawCard" class="pile"></div>
+        <div class="current-card">
+          <img v-bind:src="drawnCardUrl"/>
+        </div>
       </div>
-    </div>
-  `,
-  props: ['enabled', 'name', 'deckID'],
-  methods: {
-    drawCard: async function() {
-      if(this.enabled) {
-        const draw = await DoC.drawFromPile({ 
-          deckID: this.deckID, 
-          pileName: this.name
-        });
-        
-        console.log(draw);
-        this.drawnCardUrl = draw.cards[0].image;
-        this.remaining = draw.piles[this.name].remaining;
-        this.$emit('drawn', {
-          pile: this.name,
-          value: draw.cards[0].value
-        });
+    `,
+    props: ['enabled', 'name', 'deckID'],
+    methods: {
+      drawCard: async function() {
+        if(this.enabled) {
+          const draw = await DoC.drawFromPile({ 
+            deckID: this.deckID, 
+            pileName: this.name
+          });
+          
+          console.log(draw);
+          this.drawnCardUrl = draw.cards[0].image;
+          this.remaining = draw.piles[this.name].remaining;
+          this.$emit('drawn', {
+            pile: this.name,
+            value: draw.cards[0].value
+          });
+        }
       }
+    },
+    data: function() {
+      return {
+        drawnCardUrl: '',
+        remaining: 0
+      };
     }
-  },
-  data: function() {
-    return {
-      drawnCardUrl: '',
-      remaining: 0
-    };
-  }
- });
+   });
 
- let app = new Vue({
-  el: '#app'
- });
+   let app = new Vue({
+    el: '#app'
+  });
+}(window.Vue, window.DoC));
